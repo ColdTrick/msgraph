993b40a07c0f89adacd6ac500c5de89a3f6d04da
Jerome Bakker <jeabakker@coldtrick.com>
2016-10-27 10:55:09 +0200

    chore: basic plugin settings

--- languages/en.php
+++ languages/en.php
@@ -2,4 +2,12 @@
 
 return [
 	
+	// plugin settings
+	'msgraph:settings:client_id' => "Client/application ID",
+	'msgraph:settings:client_id:help' => "The ID of the application as reported in Azure",
+	'msgraph:settings:client_secret' => "Client secret",
+	'msgraph:settings:client_secret:help' => "The secret associated with the client ID",
+	'msgraph:settings:app_uri' => "Application URI",
+	'msgraph:settings:app_uri:help' => "The application URI as reported in Azure",
+	'' => "",
 ];
---
+++ views/default/plugins/msgraph/settings.php
@@ -0,0 +1,25 @@
+<?php
+
+/* @var $plugin \ElggPlugin */
+$plugin = elgg_extract('entity', $vars);
+
+echo elgg_view_input('text', [
+	'label' => elgg_echo('msgraph:settings:client_id'),
+	'help' => elgg_echo('msgraph:settings:client_id:help'),
+	'name' => 'params[client_id]',
+	'value' => $plugin->client_id,
+]);
+
+echo elgg_view_input('text', [
+	'label' => elgg_echo('msgraph:settings:client_secret'),
+	'help' => elgg_echo('msgraph:settings:client_secret:help'),
+	'name' => 'params[client_secret]',
+	'value' => $plugin->client_secret,
+]);
+
+echo elgg_view_input('url', [
+	'label' => elgg_echo('msgraph:settings:app_uri'),
+	'help' => elgg_echo('msgraph:settings:app_uri:help'),
+	'name' => 'params[app_uri]',
+	'value' => $plugin->app_uri,
+]);
